f0cb4805cba62c2e50a70ac20be13bf42ece7b0d
ayoubbr <ayoub1benmansour@gmail.com>
2024-11-14 11:28:41 +0100

    finalising the  sass and html code of page team

---
+++ pages/team-member.js
@@ -0,0 +1,16 @@
+async function fetchData() {
+    try {
+        let response = await fetch("../data.json");
+        if (!response.ok) {
+            throw new Error("no data");
+        }
+        let jsonData = await response.json();
+        console.log(jsonData);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        console.log("ended");
+    }
+}
+
+fetchData();
